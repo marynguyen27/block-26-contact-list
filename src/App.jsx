dcd143448cce0dc8d2482822744f245bdf5a489b
Mary Nguyen <marynguyen27@gmail.com>
2024-07-04 10:26:39 -0700

    adding contactrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import ContactList from './components/ContactList.jsx';
 
-const contacts = [
-  { name: "John Doe", email: "john.doe@example.com", phone: "123-456-7890" },
-  { name: "Jane Smith", email: "jane.smith@example.com", phone: "987-654-3210" },
+const dummyContacts = [
+  { id: 1, name: 'R2-D2', phone: '222-222-2222', email: 'r2d2@droids.com' },
+  { id: 2, name: 'C-3PO', phone: '333-333-3333', email: 'c3po@droids.com' },
+  { id: 3, name: 'BB-8', phone: '888-888-8888', email: 'bb8@droids.com' },
 ];
 
 export default function App() {
+  const [contacts, setContacts] = useState(dummyContacts);
+  console.log('Contacts: ', contacts);
   return (
     <>
-      <ContactList contacts={contacts} />
+      <ContactList contacts={dummyContacts} />
     </>
   );
 }
